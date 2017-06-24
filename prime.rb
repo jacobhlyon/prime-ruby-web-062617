ee8555ff822110970a6e16e58562dd939b434a4d
Jacob Lyon <jacobhlyon@gmail.com>
2017-06-24 01:54:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  else
+    array = (2..number - 1).to_a
+    array.each do |character|
+      if number % character == 0
+        return false
+      end
+    end
+  end
+  return true
+end
